ffc787c5e14c94066951738572af8b91190adcf0
fanzyboy <ivanananda12345@gmail.com>
2025-05-21 14:08:48 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\MovieController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [MovieController::class,'index']);
+Route::get('/movie/{id}/{slug}', [MovieController::class,'detail_movie']);
+Route::get('/movie/create', [MovieController::class,'create']);
+Route::post('/movie/store', [MovieController::class,'store']);
+
+
